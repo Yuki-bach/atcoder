87587bd41e7561fe8d71269b274529a88361bf5e
Yuki-bach <yuki20010221@icloud.com>
2023-10-13 10:58:07 -0700

    initial commit

---
+++ sample/answer.py
@@ -0,0 +1,10 @@
+import time
+start = time.perf_counter()
+
+######################
+
+
+#########################
+end = time.perf_counter()
+print(f"time: {(end-start)*1000:.3f}")
+# python3 answer.py < input.txt
