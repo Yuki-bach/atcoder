9de77c55b0e92bccad1d6b74d019b024cf1390ad
Yuki-bach <yuki20010221@icloud.com>
2023-10-21 09:48:49 -0700

    ABC270_A

---
+++ ABC270_A/answer.py
@@ -0,0 +1,14 @@
+import time
+start = time.perf_counter()
+
+######################
+A, B = map(int, input().split())
+print(A|B)
+
+
+#########################
+end = time.perf_counter()
+print(f"time: {(end-start)*1000:.3f}")
+
+## python3 ./folder/answer.py < ./folder/input.txt
+## cp -r sample folder
