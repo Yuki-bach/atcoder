e418009212b18a603aefcec513ead1e3daa57ce9
Yuki-bach <yuki20010221@icloud.com>
2024-01-10 22:32:11 -0700

    20240110

---
+++ ABC237_A/answer.py
@@ -0,0 +1,5 @@
+N = int(input())
+print("Yes" if -2**31 <= N <= 2**31 - 1 else "No")
+
+## python3 ./folder/answer.py < ./folder/input.txt
+## cp -r sample folder
---
+++ ABC238_A/answer.py
@@ -0,0 +1,5 @@
+n = int(input())
+print("Yes" if 2**n > n**2 else "No")
+
+## python3 ./folder/answer.py < ./folder/input.txt
+## cp -r sample folder
