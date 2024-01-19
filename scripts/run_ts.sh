f5b7266da6820e8878038d9e51e9e992487d127a
Yuki Ogawa <ogachan6464@gmail.com>
2024-01-19 15:56:25 -0700

    update run_ts.ch to run eslint

--- scripts/run_ts.sh
+++ scripts/run_ts.sh
@@ -7,4 +7,12 @@ fi
 
 TASK=$1
 
+## EsLint
+npx eslint solutions_ts/$TASK/answer.ts
+if [ $? -ne 0 ]; then
+    echo "ESLint found issues."
+    exit 1
+fi
+
+## Run
 npx ts-node solutions_ts/$TASK/answer.ts < solutions_ts/$TASK/input.txt
