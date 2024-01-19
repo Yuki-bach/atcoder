1d0cd05b8e0b9e8469c1162a219e954775513f97
Yuki Ogawa <ogachan6464@gmail.com>
2024-01-19 13:20:54 -0700

    update solutions_ts/answer.ts

--- solutions_ts/sample/answer.ts
+++ solutions_ts/sample/answer.ts
@@ -1,5 +1,42 @@
 import * as fs from "fs";
-const inputs = fs.readFileSync("/dev/stdin", "utf8");
 
+// Ref: https://qiita.com/cosocaf/items/255003ecec1d3badfc7b
+let inputs = "";
+let inputArray: string[];
+let currentIndex = 0;
+
+function next() {
+  return inputArray[currentIndex++];
+}
+function nextNum() {
+  return +next();
+}
+// JavaScriptのnumber型は2^53(≒9*10^15)以上の数を正確に表すことができない。
+function nextBigInt() {
+  return BigInt(next());
+}
+function nexts(length: number) {
+  const arr = [];
+  for(let i = 0; i < length; ++i) arr[i] = next();
+  return arr;
+}
+function nextNums(length: number) {
+  const arr = [];
+  for(let i = 0; i < length; ++i) arr[i] = nextNum();
+  return arr;
+}
+function nextBigInts(length: number) {
+  const arr = [];
+  for(let i = 0; i < length; ++i) arr[i] = nextBigInt();
+  return arr;
+}
+
+inputs = fs.readFileSync("/dev/stdin", "utf8");
+inputArray = inputs.split(/\s/);
+main();
+
+function main() {
+  
+}
 
 // $ npm run complete --task=sample
