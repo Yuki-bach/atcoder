bcea91878c8aa2bef5e34c6283068d3038fcbac7
Yuki Ogawa <ogachan6464@gmail.com>
2024-01-26 16:09:14 -0700

    20240126

---
+++ solutions_ts/ABC223_B/answer.ts
@@ -0,0 +1,51 @@
+import * as fs from "fs";
+
+let inputs = "";
+let inputArray: string[];
+let currentIndex = 0;
+
+function next() {
+  return inputArray[currentIndex++];
+}
+function nextNum() {
+  return +next();
+}
+// JavaScriptのnumber型は2^53(≒9*10^15)以上の数を正確に表すことができない。
+function nextBigInt() {
+  return BigInt(next());
+}
+function nexts(length: number) {
+  const arr = [];
+  for(let i = 0; i < length; ++i) arr[i] = next();
+  return arr;
+}
+function nextNums(length: number) {
+  const arr = [];
+  for(let i = 0; i < length; ++i) arr[i] = nextNum();
+  return arr;
+}
+function nextBigInts(length: number) {
+  const arr = [];
+  for(let i = 0; i < length; ++i) arr[i] = nextBigInt();
+  return arr;
+}
+
+inputs = fs.readFileSync("/dev/stdin", "utf8");
+inputArray = inputs.split(/\s/);
+main();
+
+function main() {
+  let S = next();
+  let ansMin = S;
+  let ansMax = S;
+
+  for (let i = 0; i < S.length; i++) {
+    S = S.substring(1) + S.charAt(0);
+    if (S < ansMin) ansMin = S;
+    if (S > ansMax) ansMax = S;
+  }
+  console.log(ansMin);
+  console.log(ansMax);
+}
+
+// $ npm run complete --task=sample
