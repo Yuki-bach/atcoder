c2944ac21869e9e555675b14ce6c222598cc8e1c
Yuki Ogawa <ogachan6464@gmail.com>
2024-01-27 23:35:30 -0700

    20240127

---
+++ solutions_ts/ABC338_A/answer.ts
@@ -0,0 +1,44 @@
+import * as fs from "fs";
+
+let inputs = "";
+let inputArray: string[];
+let currentIndex = 0;
+
+function next() {
+  return inputArray[currentIndex++];
+}
+function nextNum() {
+  return +next();
+}
+// JavaScriptのnumber型は2^53(≒9*10^15)以上の数を正確に表すことができない。
+function nextBigInt() {
+  return BigInt(next());
+}
+function nexts(length: number) {
+  const arr = [];
+  for(let i = 0; i < length; ++i) arr[i] = next();
+  return arr;
+}
+function nextNums(length: number) {
+  const arr = [];
+  for(let i = 0; i < length; ++i) arr[i] = nextNum();
+  return arr;
+}
+function nextBigInts(length: number) {
+  const arr = [];
+  for(let i = 0; i < length; ++i) arr[i] = nextBigInt();
+  return arr;
+}
+
+inputs = fs.readFileSync("/dev/stdin", "utf8");
+inputArray = inputs.split(/\s/);
+main();
+
+function main() {
+  const S = next();
+  const checker :boolean = S[0] === S[0].toUpperCase()
+    && S.slice(1) === S.slice(1).toLowerCase();
+  console.log(checker ? "Yes" : "No");
+}
+
+// $ npm run complete --task=sample
