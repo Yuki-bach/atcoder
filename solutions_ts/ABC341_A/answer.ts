35097ffb05f610ad9e637f57c8c390f4238299e9
Yuki Ogawa <ogachan6464@gmail.com>
2024-02-17 19:28:17 -0700

    20240217

---
+++ solutions_ts/ABC341_A/answer.ts
@@ -0,0 +1,46 @@
+import * as fs from "fs";
+
+let inputs = "";
+let inputArray: string[];
+let currentIndex = 0;
+
+function next() {
+  return inputArray[currentIndex++];
+}
+function nextNum() {
+  return +next();
+}
+// JavaScriptのnumber型は2^53(≒9*10^15)以上の数を正確に表すことができない。
+function nextBigInt() {
+  return BigInt(next());
+}
+function nexts(length: number) {
+  const arr = [];
+  for (let i = 0; i < length; ++i) arr[i] = next();
+  return arr;
+}
+function nextNums(length: number) {
+  const arr = [];
+  for (let i = 0; i < length; ++i) arr[i] = nextNum();
+  return arr;
+}
+function nextBigInts(length: number) {
+  const arr = [];
+  for (let i = 0; i < length; ++i) arr[i] = nextBigInt();
+  return arr;
+}
+
+inputs = fs.readFileSync("/dev/stdin", "utf8");
+inputArray = inputs.split(/\s/);
+main();
+
+function main() {
+  const N: number = nextNum();
+  let ans: string = "1";
+  for (let i = 0; i < N; i++) {
+    ans += "01";
+  }
+  console.log(ans);
+}
+
+// $ npm run complete --task=sample
---
+++ solutions_ts/ABC341_B/answer.ts
@@ -0,0 +1,50 @@
+import * as fs from "fs";
+
+let inputs = "";
+let inputArray: string[];
+let currentIndex = 0;
+
+function next() {
+  return inputArray[currentIndex++];
+}
+function nextNum() {
+  return +next();
+}
+// JavaScriptのnumber型は2^53(≒9*10^15)以上の数を正確に表すことができない。
+function nextBigInt() {
+  return BigInt(next());
+}
+function nexts(length: number) {
+  const arr = [];
+  for (let i = 0; i < length; ++i) arr[i] = next();
+  return arr;
+}
+function nextNums(length: number) {
+  const arr = [];
+  for (let i = 0; i < length; ++i) arr[i] = nextNum();
+  return arr;
+}
+function nextBigInts(length: number) {
+  const arr = [];
+  for (let i = 0; i < length; ++i) arr[i] = nextBigInt();
+  return arr;
+}
+
+inputs = fs.readFileSync("/dev/stdin", "utf8");
+inputArray = inputs.split(/\s/);
+main();
+
+function main() {
+  const N: number = nextNum();
+  const A: number[] = nextNums(N);
+
+  for (let i = 0; i < N - 1; i++) {
+    const [S, T] = nextNums(2);
+    const count: number = Math.floor(A[i] / S);
+    A[i] -= S * count;
+    A[i + 1] += T * count;
+  }
+  console.log(A.at(-1));
+}
+
+// $ npm run complete --task=sample
