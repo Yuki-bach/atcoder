887b611b4847f58d171218958c5f935f1042ddfb
Yuki Ogawa <ogachan6464@gmail.com>
2024-04-04 12:55:13 -0700

    20240404

---
+++ solutions_ts/ABC347_B/answer.ts
@@ -0,0 +1,51 @@
+import * as fs from "fs";
+
+let inputs = "";
+let inputArray: string[];
+let currentIndex = 0;
+
+function next() {
+  return inputArray[currentIndex++];
+}
+function nextNum() {
+  return +next();
+}
+// JavaScriptのnumber型は2^53(≒9*10^15)以上の数を正確に表すことができない。
+function nextBigInt() {
+  return BigInt(next());
+}
+function nexts(length: number) {
+  const arr = [];
+  for (let i = 0; i < length; ++i) arr[i] = next();
+  return arr;
+}
+function nextNums(length: number) {
+  const arr = [];
+  for (let i = 0; i < length; ++i) arr[i] = nextNum();
+  return arr;
+}
+function nextBigInts(length: number) {
+  const arr = [];
+  for (let i = 0; i < length; ++i) arr[i] = nextBigInt();
+  return arr;
+}
+
+inputs = fs.readFileSync("/dev/stdin", "utf8");
+inputArray = inputs.split(/\s/);
+main();
+
+function main() {
+  const S: string = next();
+  const substrings = new Set<string>();
+
+  for (let i = 0; i < S.length; i++) {
+    for (let j = i + 1; j <= S.length; j++) {
+      const substring = S.substring(i, j);
+      substrings.add(substring);
+    }
+  }
+  
+  console.log(substrings.size);
+}
+
+// $ npm run complete --task=sample
