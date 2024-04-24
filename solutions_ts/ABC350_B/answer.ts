b046f889d870b4b6f12a5eaed663994ba95e5b5f
Yuki Ogawa <ogachan6464@gmail.com>
2024-04-24 11:05:50 -0700

    20240424

---
+++ solutions_ts/ABC350_B/answer.ts
@@ -0,0 +1,53 @@
+import * as fs from "fs";
+
+let inputs = "";
+let inputArray: string[];
+let currentIndex = 0;
+
+function next() {
+  return inputArray[currentIndex++];
+}
+function nextNum() {
+  return +next();
+}
+// JavaScriptのnumber型は2^53(≒9*10^15)以上の数を正確に表すことができない。
+function nextBigInt() {
+  return BigInt(next());
+}
+function nexts(length: number) {
+  const arr = [];
+  for(let i = 0; i < length; ++i) arr[i] = next();
+  return arr;
+}
+function nextNums(length: number) {
+  const arr = [];
+  for(let i = 0; i < length; ++i) arr[i] = nextNum();
+  return arr;
+}
+function nextBigInts(length: number) {
+  const arr = [];
+  for(let i = 0; i < length; ++i) arr[i] = nextBigInt();
+  return arr;
+}
+
+inputs = fs.readFileSync("/dev/stdin", "utf8");
+inputArray = inputs.split(/\s/);
+main();
+
+function main() {
+  const [N, Q]: number[] = nextNums(2);
+  const T: number[] = nextNums(Q);
+  
+  const teeth: number[] = Array(N).fill(1);
+
+  for (const t of T) {
+    teeth[t - 1] *= -1;
+  }
+
+  const ans: number = teeth
+    .filter(t => t === 1)
+    .reduce((acc, cur) => acc + cur, 0);
+  console.log(ans);
+}
+
+// $ npm run complete --task=sample
